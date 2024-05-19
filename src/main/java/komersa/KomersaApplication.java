8d65e1dee123156f0df6fdbc0d05184440f8fe77
fanambynana <hei.fanambinana@gmail.com>
2024-05-19 20:21:51 +0300

    spring gen

---
+++ src/main/java/komersa/KomersaApplication.java
@@ -0,0 +1,13 @@
+package komersa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KomersaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KomersaApplication.class, args);
+    }
+
+}
