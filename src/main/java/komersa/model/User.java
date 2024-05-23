a52a6b86495ff47d96c1293fa7d69db13e291a04
fanambynana <hei.fanambinana@gmail.com>
2024-05-23 15:17:39 +0300

    feat(user): add model

---
+++ src/main/java/komersa/model/User.java
@@ -0,0 +1,23 @@
+package komersa.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity(name = "'user'")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "usr_id")
+    private long usrId;
+    @Column(name = "usr_fname")
+    private String usrFname;
+    @Column(name = "usr_email")
+    private String usrEmail;
+    @Column(name = "usr_pswd")
+    private String usrPswd;
+}
