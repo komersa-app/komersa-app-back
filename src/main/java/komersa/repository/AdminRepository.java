f3f9f42355912b950baf028c01271f11d28d721a
tokiniaina <hei.tokiniaina@gmail.com>
2024-05-28 21:30:30 +0300

    feat: generated repository of all model

---
+++ src/main/java/komersa/repository/AdminRepository.java
@@ -0,0 +1,8 @@
+package komersa.repository;
+
+import komersa.model.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminRepository extends JpaRepository<Admin, int> {
+
+}
---
+++ src/main/java/komersa/repository/priceRepository.java
@@ -0,0 +1,8 @@
+package komersa.repository;
+
+import komersa.model.price;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface priceRepository extends JpaRepository<price, Long> {
+
+}
