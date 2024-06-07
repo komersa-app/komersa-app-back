95b14ad0373625886b9827ccb98492048a49f669
fanambynana <hei.fanambinana@gmail.com>
2024-06-07 12:13:57 +0300

    chore: by pass auth -- login only

--- src/main/java/komersa/controller/CarController.java
+++ src/main/java/komersa/controller/CarController.java
@@ -30,8 +30,7 @@ public class CarController {
     @ApiResponse(responseCode = "201", description = "Car saved successfully")
     @ApiResponse(responseCode = "400", description = "Invalid input")
     @ApiResponse(responseCode = "404", description = "Invalid foreign key that is not found")
-    public ResponseEntity<CarDtoResponse> createCar(@RequestHeader(required = false, value = "Authorization") String token, @Valid @RequestBody CarDtoRequest carDtoRequest) {
-        verifyToken(token);
+    public ResponseEntity<CarDtoResponse> createCar(@Valid @RequestBody CarDtoRequest carDtoRequest) {
         Car car = CarDtoMapper.toModel(carDtoRequest);
         car = carService.create(car);
         return new ResponseEntity<>(CarDtoMapper.toResponse(car), HttpStatus.CREATED);
@@ -60,8 +59,7 @@ public class CarController {
     @ApiResponse(responseCode = "201", description = "Car updated successfully")
     @ApiResponse(responseCode = "400", description = "Invalid input")
     @ApiResponse(responseCode = "404", description = "Car with such an Id not found or invalid foreign key that is not found")
-    public ResponseEntity<CarDtoResponse> updateCar(@RequestHeader(required = false, value = "Authorization") String token, @PathVariable("id") Long id, @Valid @RequestBody CarDtoRequest carDtoRequest) {
-        verifyToken(token);
+    public ResponseEntity<CarDtoResponse> updateCar(@PathVariable("id") Long id, @Valid @RequestBody CarDtoRequest carDtoRequest) {
         Car car = CarDtoMapper.toModel(carDtoRequest);
         car = carService.updateById(id, car);
         return new ResponseEntity<>(CarDtoMapper.toResponse(car), HttpStatus.CREATED);
@@ -70,8 +68,7 @@ public class CarController {
     @DeleteMapping("/{id}")
     @Operation(summary = "Delete an car", description = "Delete an car by id")
     @ApiResponse(responseCode = "204", description = "Car deleted successfully")
-    public ResponseEntity<Boolean> deleteCar(@RequestHeader(required = false, value = "Authorization") String token, @PathVariable("id") Long id) {
-        verifyToken(token);
+    public ResponseEntity<Boolean> deleteCar(@PathVariable("id") Long id) {
         return new ResponseEntity<>(carService.deleteById(id), HttpStatus.NO_CONTENT);
     }
 }
